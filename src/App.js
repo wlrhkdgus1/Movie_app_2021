4ce3a4cc19598d699e033c6d46b2090d6dffb8e2
Gwang Hyun <gwgw4578@gmail.com>
2021-04-01 19:45:06 +0900

    21 04 01 componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component{
-  state={
-    count:0
+  state = {
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  }
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1}));
+  componentDidMount(){ //처음에 우리가 render를 하면 호출되는 life cycle method
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render(){
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button> 
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div> // true면 Loading false면 We are ready
+   
   }
 }
 
